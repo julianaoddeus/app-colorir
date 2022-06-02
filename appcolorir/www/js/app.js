09f0a10a38c76b86a39075fce01080b77d24e8ec
Juliana <64862169+julianaoddeus@users.noreply.github.com>
2022-06-01 22:22:47 -0300

    Update final

--- appcolorir/www/js/app.js
+++ appcolorir/www/js/app.js
@@ -1,5 +1,9 @@
 function MudarFundo(){
-    let cor = 'red'
+   let corR = Math.floor(Math.random() * 255)
+   let corG = Math.floor(Math.random() * 255)
+   let corB = Math.floor(Math.random() * 255)
+   let cor =  `rgb(${corR},${corG},${corB})`
+  
     document.body.style.backgroundColor = cor;
 }
 
